54056a3f12cdf553f247983381a3ee43dab94432
Danil <Kutfilloev.danil@yandex.ru>
2022-08-05 12:29:12 +0500

    прогу сделали более красивой

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// написать программу которая принимает число и выдает его в квадрат.
+// (число умноженное само на себя).
+
+Console.WriteLine ("напишите число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+int result = number*number;
+Console.WriteLine ("квадрат числа равен: ");
+Console.WriteLine (result);
